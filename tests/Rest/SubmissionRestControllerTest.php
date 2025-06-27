b1dc010e5941f6996d3e3454cdc2b7f38df22f14
myspc-development <myspchosting@gmail.com>
2025-06-27 10:28:20 +0300

    Secure submission endpoint

--- tests/Rest/SubmissionRestControllerTest.php
+++ tests/Rest/SubmissionRestControllerTest.php
@@ -1,4 +1,40 @@
 <?php
+namespace ArtPulse\Rest;
+
+// --- Stubs for WordPress functions used in the controller ---
+function current_user_can(string $cap) {
+    return \ArtPulse\Rest\Tests\SubmissionStub::$can;
+}
+
+function wp_verify_nonce($nonce, $action) {
+    return \ArtPulse\Rest\Tests\SubmissionStub::$nonce_valid && $nonce === 'good' && $action === 'wp_rest';
+}
+
+class WP_REST_Request {
+    private array $params;
+    private array $json;
+    private array $headers;
+
+    public function __construct(array $params = [], array $json = [], array $headers = []) {
+        $this->params  = $params;
+        $this->json    = $json;
+        $this->headers = array_change_key_case($headers, CASE_LOWER);
+    }
+
+    public function get_param(string $key) {
+        return $this->params[$key] ?? null;
+    }
+
+    public function get_json_params() {
+        return $this->json;
+    }
+
+    public function get_header(string $key) {
+        $key = strtolower($key);
+        return $this->headers[$key] ?? '';
+    }
+}
+
 namespace ArtPulse\Rest\Tests;
 
 use PHPUnit\Framework\TestCase;
@@ -28,5 +64,46 @@ class SubmissionRestControllerTest extends TestCase
         $this->assertSame('string', $args['artist_name']['type']);
         $this->assertArrayHasKey('ead_org_name', $args);
         $this->assertSame('string', $args['ead_org_name']['type']);
+    }
+
+    public function test_check_permissions_valid_nonce_and_capability(): void
+    {
+        SubmissionStub::reset();
+        $req = new \ArtPulse\Rest\WP_REST_Request([], [], ['X-WP-Nonce' => 'good']);
+        $ref = new \ReflectionMethod(SubmissionRestController::class, 'check_permissions');
+        $ref->setAccessible(true);
+        $this->assertTrue($ref->invoke(null, $req));
+    }
+
+    public function test_check_permissions_fails_with_invalid_nonce(): void
+    {
+        SubmissionStub::reset();
+        SubmissionStub::$nonce_valid = false;
+        $req = new \ArtPulse\Rest\WP_REST_Request([], [], ['X-WP-Nonce' => 'bad']);
+        $ref = new \ReflectionMethod(SubmissionRestController::class, 'check_permissions');
+        $ref->setAccessible(true);
+        $this->assertFalse($ref->invoke(null, $req));
+    }
+
+    public function test_check_permissions_fails_without_capability(): void
+    {
+        SubmissionStub::reset();
+        SubmissionStub::$can = false;
+        $req = new \ArtPulse\Rest\WP_REST_Request([], [], ['X-WP-Nonce' => 'good']);
+        $ref = new \ReflectionMethod(SubmissionRestController::class, 'check_permissions');
+        $ref->setAccessible(true);
+        $this->assertFalse($ref->invoke(null, $req));
+    }
+}
+
+class SubmissionStub
+{
+    public static bool $can = true;
+    public static bool $nonce_valid = true;
+
+    public static function reset(): void
+    {
+        self::$can = true;
+        self::$nonce_valid = true;
     }
 }
