ff0ee457ce0079efdc98446d6372f77d76fbec6b
myspc-development <myspchosting@gmail.com>
2025-06-27 11:51:59 +0300

    Add portfolio gallery support

---
+++ templates/salient/content-portfolio.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Single template for portfolio posts, using Salient portfolio wrappers.
+ *
+ * Place this file in:
+ *   wp-content/plugins/artpulse-management-plugin/templates/salient/content-portfolio.php
+ */
+
+get_header(); ?>
+
+<div id="nectar-outer">
+  <div class="container-wrap">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <?php
+          while ( have_posts() ) : the_post();
+
+            // Featured image
+            if ( has_post_thumbnail() ) {
+              echo '<div class="nectar-portfolio-single-media">';
+              the_post_thumbnail( 'full', [ 'class' => 'img-responsive' ] );
+              echo '</div>';
+            }
+
+            // Additional gallery images
+            $gallery_ids = get_post_meta( get_the_ID(), '_ap_submission_images', true );
+            if ( is_array( $gallery_ids ) && count( $gallery_ids ) > 1 ) {
+                          echo '<div class="event-gallery swiper">';
+              echo '<div class="swiper-wrapper">';
+              foreach ( array_slice( $gallery_ids, 1 ) as $img_id ) {
+                echo '<div class="swiper-slide">' . wp_get_attachment_image( $img_id, 'large' ) . '</div>';
+              }
+              echo '</div><div class="swiper-pagination"></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div></div>';
+            }
+
+            // Title
+            echo '<h1 class="entry-title">'. get_the_title() .'</h1>';
+
+            // Content
+            echo '<div class="entry-content">';
+            the_content();
+            echo '</div>';
+
+            // Event meta
+            $date     = get_post_meta( get_the_ID(), '_ap_event_date', true );
+            $location = get_post_meta( get_the_ID(), '_ap_event_location', true );
+
+            if ( $date || $location ) {
+              echo '<ul class="portfolio-meta">';
+              if ( $date ) {
+                echo '<li><strong>'. esc_html__( 'Date:', 'artpulse' ) .'</strong> '. esc_html( $date ) .'</li>';
+              }
+              if ( $location ) {
+                echo '<li><strong>'. esc_html__( 'Location:', 'artpulse' ) .'</strong> '. esc_html( $location ) .'</li>';
+              }
+              echo '</ul>';
+            }
+
+          endwhile;
+          ?>
