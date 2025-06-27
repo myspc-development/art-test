7093ed1c464f0860879b7702da20f0e02249ed77
myspc-development <myspchosting@gmail.com>
2025-06-27 08:48:53 +0300

    Refactor REST controllers to defer route registration

--- src/Community/FollowRestController.php
+++ src/Community/FollowRestController.php
@@ -10,6 +10,11 @@ class FollowRestController
 {
     public static function register(): void
     {
+        add_action('rest_api_init', [self::class, 'register_routes']);
+    }
+
+    public static function register_routes(): void
+    {
         register_rest_route('artpulse/v1', '/follows', [
             'methods'             => 'POST',
             'callback'            => [self::class, 'add_follow'],
