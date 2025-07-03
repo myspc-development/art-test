cbb9ff7d63c43968dbc7c3f74f671d1b0abf33b2
myspc-development <myspchosting@gmail.com>
2025-07-03 08:34:54 +0300

    Add REST taxonomy endpoint tests

---
+++ tests/Rest/TaxonomyEndpointsTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace ArtPulse\Rest\Tests;
+
+use WP_REST_Request;
+use ArtPulse\Taxonomies\TaxonomiesRegistrar;
+
+/**
+ * @group restapi
+ */
+class TaxonomyEndpointsTest extends \WP_UnitTestCase
+{
+    public function set_up(): void
+    {
+        parent::set_up();
+        TaxonomiesRegistrar::register();
+        do_action('init');
+        wp_insert_term('Exhibition', 'event_type');
+        wp_insert_term('Painting', 'artwork_style');
+        do_action('rest_api_init');
+    }
+
+    public function test_event_type_endpoint_returns_terms(): void
+    {
+        $req = new WP_REST_Request('GET', '/wp/v2/event_type');
+        $res = rest_get_server()->dispatch($req);
+        $this->assertSame(200, $res->get_status());
+        $this->assertIsArray($res->get_data());
+    }
+
+    public function test_artwork_style_endpoint_returns_terms(): void
+    {
+        $req = new WP_REST_Request('GET', '/wp/v2/artwork_style');
+        $res = rest_get_server()->dispatch($req);
+        $this->assertSame(200, $res->get_status());
+        $this->assertIsArray($res->get_data());
+    }
+}
