f7caf41f78e3f38882d44fc234e933ec3c4c5a57
myspc-development <myspchosting@gmail.com>
2025-07-05 09:33:19 +0300

    Display favorites list on dashboard

--- templates/dashboard-member.php
+++ templates/dashboard-member.php
@@ -1,4 +1,5 @@
 <?php
+use ArtPulse\Community\FavoritesManager;
 /**
  * Member dashboard template.
  */
@@ -68,10 +69,30 @@
     </div>
     <div class="dashboard-card" data-widget="favorites">
         <h2 id="favorites"><?php _e('My Favorites','artpulse'); ?></h2>
-        <div id="ap-favorite-events"></div>
-        <div id="ap-favorite-artists"></div>
-        <div id="ap-favorite-orgs"></div>
-        <div id="ap-favorite-artworks"></div>
+        <?php
+        use ArtPulse\Community\FavoritesManager;
+
+        $user_id   = get_current_user_id();
+        $favorites = FavoritesManager::get_favorites($user_id);
+
+        if (empty($favorites)) {
+            echo '<p>' . esc_html__('You haven’t favorited any content yet.', 'artpulse') . '</p>';
+        } else {
+            foreach (['event', 'artist', 'organization', 'artwork'] as $type) {
+                if (!empty($favorites[$type])) {
+                    echo '<div class="favorite-group favorite-group-' . esc_attr($type) . '">';
+                    echo '<h3>' . esc_html(ucfirst($type)) . 's</h3>';
+                    echo '<ul class="favorites-list">';
+                    foreach ($favorites[$type] as $post_id) {
+                        $title = get_the_title($post_id);
+                        $link  = get_permalink($post_id);
+                        echo '<li><a href="' . esc_url($link) . '">' . esc_html($title) . '</a></li>';
+                    }
+                    echo '</ul></div>';
+                }
+            }
+        }
+        ?>
     </div>
     <div class="dashboard-card" data-widget="rsvps">
         <h2 id="rsvps"><?php _e('My RSVPs','artpulse'); ?></h2>
