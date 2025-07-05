8f1bf3e8320a8e64a9108da2d393c81cd9718679
myspc-development <myspchosting@gmail.com>
2025-07-05 14:48:19 +0300

    Add PHPUnit tests for Blocks, Ajax, and Search modules

---
+++ tests/Blocks/FilteredListShortcodeBlockTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace ArtPulse\Blocks;
+
+function do_shortcode($code)
+{
+    return 'SC:' . $code;
+}
+
+namespace ArtPulse\Blocks\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ArtPulse\Blocks\FilteredListShortcodeBlock;
+
+class FilteredListShortcodeBlockTest extends TestCase
+{
+    public function test_render_callback_builds_shortcode(): void
+    {
+        $html = FilteredListShortcodeBlock::render_callback([
+            'postType' => 'artpulse_artist',
+            'taxonomy' => 'artist_specialty',
+            'terms' => 'painting,sculpture',
+            'postsPerPage' => 10,
+        ]);
+        $expected = 'SC:[ap_filtered_list post_type="artpulse_artist" taxonomy="artist_specialty" terms="painting,sculpture" posts_per_page="10"]';
+        $this->assertSame($expected, $html);
+    }
+}
---
+++ tests/Search/MetaFullTextSearchTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace ArtPulse\Search\Tests;
+
+use WP_REST_Request;
+use ArtPulse\Search\MetaFullTextSearch;
+
+class MetaFullTextSearchTest extends \WP_UnitTestCase
+{
+    public function test_rest_filter_adds_meta_query_for_valid_key(): void
+    {
+        $req = new WP_REST_Request('GET', '/');
+        $req->set_param('meta_key', 'artist_name');
+        $req->set_param('meta_value', 'john');
+        $args = MetaFullTextSearch::rest_meta_search_filter([], $req, 'artpulse_artist');
+        $this->assertArrayHasKey('meta_query', $args);
+        $this->assertSame('artist_name', $args['meta_query'][0]['key']);
+        $this->assertSame('john', $args['meta_query'][0]['value']);
+    }
+
+    public function test_rest_filter_ignores_invalid_key(): void
+    {
+        $req = new WP_REST_Request('GET', '/');
+        $req->set_param('meta_key', 'invalid');
+        $req->set_param('meta_value', 'val');
+        $args = MetaFullTextSearch::rest_meta_search_filter([], $req, 'artpulse_artist');
+        $this->assertArrayNotHasKey('meta_query', $args);
+    }
+}
