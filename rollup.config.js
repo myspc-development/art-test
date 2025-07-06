67420a411e3990d3feeff0185b2f0c0b0d1b523a
myspc-development <myspchosting@gmail.com>
2025-07-06 15:14:32 +0300

    chore(build): bundle lucide icons

--- rollup.config.js
+++ rollup.config.js
@@ -19,12 +19,11 @@ function createConfig(input, file, name, globals = {}, external = Object.keys(gl
 
 module.exports = [
   createConfig('assets/js/OrganizationSubmissionForm.jsx', 'assets/js/ap-org-submission.js', 'APOrgSubmission', { react: 'React' }),
-  createConfig('assets/js/SidebarMenu.jsx', 'assets/js/sidebar-menu.js', 'APSidebarMenu', { react: 'React', 'lucide-react': 'lucideReact' }),
+  createConfig('assets/js/SidebarMenu.jsx', 'assets/js/sidebar-menu.js', 'APSidebarMenu', { react: 'React' }),
   createConfig('assets/js/dashboard.jsx', 'assets/js/ap-dashboard.js', 'APDashboard', {
     react: 'React',
     'react-dom': 'ReactDOM',
     'assets/js/SidebarMenu.jsx': 'APSidebarMenu',
-    'assets/js/rolesMenus.js': 'rolesMenus',
-    'lucide-react': 'lucideReact'
-  }, ['react', 'react-dom', 'assets/js/SidebarMenu.jsx', 'assets/js/rolesMenus.js', 'lucide-react']),
+    'assets/js/rolesMenus.js': 'rolesMenus'
+  }, ['react', 'react-dom', 'assets/js/SidebarMenu.jsx', 'assets/js/rolesMenus.js']),
 ];
