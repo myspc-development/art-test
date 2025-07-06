fb367d6ff4c4efe54efddfed453e9aaa978b0cdb
myspc-development <myspchosting@gmail.com>
2025-07-06 20:50:42 +0300

    Add ProfileMetrics REST controller test

---
+++ tests/Rest/ProfileMetricsControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace ArtPulse\Rest\Tests;
+
+use WP_REST_Request;
+use ArtPulse\Rest\ProfileMetricsController;
+use ArtPulse\Core\ProfileMetrics;
+
+/**
+ * @group restapi
+ */
+class ProfileMetricsControllerTest extends \WP_UnitTestCase
+{
+    private int $user_id;
+
+    public function set_up(): void
+    {
+        parent::set_up();
+        ProfileMetrics::install_table();
+        $this->user_id = self::factory()->user->create();
+        wp_set_current_user($this->user_id);
+        ProfileMetricsController::register();
+        do_action('rest_api_init');
+    }
+
+    public function test_get_metrics_returns_expected_data(): void
+    {
+        $yesterday = date('Y-m-d', strtotime('-1 day'));
+        add_filter('current_time', function ($time, $type) use ($yesterday) {
+            return $type === 'Y-m-d' ? $yesterday : $time;
+        }, 10, 2);
+        ProfileMetrics::log_metric($this->user_id, 'view');
+        remove_all_filters('current_time');
+
+        ProfileMetrics::log_metric($this->user_id, 'view');
+
+        $req = new WP_REST_Request('GET', "/artpulse/v1/profile-metrics/{$this->user_id}");
+        $req->set_param('metric', 'view');
+        $req->set_param('days', 2);
+        $res = rest_get_server()->dispatch($req);
+
+        $this->assertSame(200, $res->get_status());
+        $data = $res->get_data();
+        $today = date('Y-m-d');
+        $this->assertSame([$yesterday, $today], $data['days']);
+        $this->assertSame([1, 1], $data['counts']);
+    }
+}
