bed9c506db9c1485a5238bb2d771c6afe2ba1d5d
myspc-development <myspchosting@gmail.com>
2025-07-08 17:06:56 +0300

    Wrap user dashboard widgets in sortable container

--- templates/dashboard-organization.php
+++ templates/dashboard-organization.php
@@ -6,7 +6,9 @@ get_header();
 
 <div class="ap-dashboard-wrap organization-dashboard">
   <h2>🏢 Organization Dashboard</h2>
-  <?php DashboardWidgetTools::render_user_dashboard(get_current_user_id()); ?>
+  <div id="ap-user-dashboard">
+    <?php DashboardWidgetTools::render_user_dashboard(get_current_user_id()); ?>
+  </div>
 </div>
 
 <?php get_footer(); ?>
