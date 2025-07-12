09a1f82a32482239d7e2fccf73927f344035bade
myspc-development <myspchosting@gmail.com>
2025-07-12 15:07:11 +0300

    Add missing scaffold files

---
+++ templates/widgets/widget-spotlights.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Spotlight widget with optional pinning (Sprint 2)
+ */
+?>
+<div class="ap-widget">
+  <div class="ap-widget-header">🌟 Spotlights</div>
+  <div class="ap-widget-body" id="ap-spotlight-list"></div>
+</div>
