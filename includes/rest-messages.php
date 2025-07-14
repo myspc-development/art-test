8bb78944fde4ad082a7a1d553a24fb0e65281fa4
myspc-development <myspchosting@gmail.com>
2025-07-14 19:50:11 +0300

    Add basic internal messaging system

---
+++ includes/rest-messages.php
@@ -0,0 +1,38 @@
+<?php
+use ArtPulse\Messages\AP_Message;
+
+add_action('rest_api_init', function () {
+    register_rest_route('artpulse/v1', '/messages/send', [
+        'methods'  => 'POST',
+        'callback' => function ($req) {
+            $sender  = get_current_user_id();
+            $receiver = absint($req['receiver_id']);
+            $content  = sanitize_textarea_field($req['content']);
+
+            if (!current_user_can('read') || !$receiver || empty($content)) {
+                return new WP_Error('invalid', 'Invalid message', ['status' => 403]);
+            }
+
+            $id = AP_Message::send($sender, $receiver, $content);
+            return rest_ensure_response(['message_id' => $id]);
+        },
+        'permission_callback' => '__return_true',
+    ]);
+
+    register_rest_route('artpulse/v1', '/messages/inbox', [
+        'methods'  => 'GET',
+        'callback' => function () {
+            return rest_ensure_response(AP_Message::get_inbox(get_current_user_id()));
+        },
+        'permission_callback' => '__return_true',
+    ]);
+
+    register_rest_route('artpulse/v1', '/messages/read/(?P<id>\d+)', [
+        'methods'  => 'POST',
+        'callback' => function ($req) {
+            AP_Message::mark_read((int)$req['id'], get_current_user_id());
+            return rest_ensure_response(['status' => 'read']);
+        },
+        'permission_callback' => '__return_true',
+    ]);
+});
---
+++ tests/Rest/APMessageTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace ArtPulse\Rest\Tests;
+
+use ArtPulse\Messages\AP_Message;
+
+class APMessageTest extends \WP_UnitTestCase {
+    public function test_send_and_receive_message() {
+        $sender = self::factory()->user->create();
+        $receiver = self::factory()->user->create();
+        $id = AP_Message::send($sender, $receiver, 'Test message');
+        $inbox = AP_Message::get_inbox($receiver);
+        $this->assertNotEmpty($inbox);
+        $this->assertEquals('Test message', $inbox[0]->content);
+    }
+}
