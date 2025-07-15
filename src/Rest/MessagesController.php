b049a18dbf60e4bc69cdbcbb3e4e7b2a3820ecf5
myspc-development <myspchosting@gmail.com>
2025-07-15 07:15:49 +0300

    Add modal reply interface for dashboard messages

---
+++ src/Rest/MessagesController.php
@@ -0,0 +1,29 @@
+<?php
+namespace ArtPulse\Rest;
+
+use WP_REST_Request;
+
+class MessagesController
+{
+    public static function register(): void
+    {
+        add_action('rest_api_init', [self::class, 'register_routes']);
+    }
+
+    public static function register_routes(): void
+    {
+        register_rest_route('artpulse/v1', '/messages/(?P<id>\d+)/reply', [
+            'methods'             => 'POST',
+            'permission_callback' => fn () => current_user_can('read'),
+            'callback'            => [self::class, 'reply'],
+        ]);
+    }
+
+    public static function reply(WP_REST_Request $request)
+    {
+        $msg_id = (int) $request['id'];
+        $content = sanitize_text_field($request->get_param('message'));
+        // Save to DB or email logic here...
+        return rest_ensure_response(['status' => 'sent']);
+    }
+}
