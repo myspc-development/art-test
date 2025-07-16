b0714f3bcf9a8364e83f7c7a91bf4fb93bac0ed3
myspc-development <myspchosting@gmail.com>
2025-07-16 10:34:31 +0300

    test: add org roles matrix rest controller tests

---
+++ tests/Rest/OrgRolesMatrixRestTest.php
@@ -0,0 +1,58 @@
+<?php
+namespace ArtPulse\Rest\Tests;
+
+use WP_REST_Request;
+use ArtPulse\Rest\OrgRolesController;
+
+/**
+ * @group restapi
+ */
+class OrgRolesMatrixRestTest extends \WP_UnitTestCase
+{
+    private int $admin;
+    private int $user;
+    private int $org;
+
+    public function set_up(): void
+    {
+        parent::set_up();
+        $this->org = self::factory()->post->create(['post_type' => 'artpulse_org']);
+        $this->admin = self::factory()->user->create(['role' => 'administrator']);
+        $this->user  = self::factory()->user->create(['role' => 'subscriber']);
+        update_user_meta($this->admin, 'ap_organization_id', $this->org);
+        update_user_meta($this->user, 'ap_organization_id', $this->org);
+
+        OrgRolesController::register();
+        do_action('rest_api_init');
+
+        wp_set_current_user($this->admin);
+    }
+
+    public function test_get_roles_for_org_returns_data(): void
+    {
+        $req = new WP_REST_Request('GET', '/artpulse/v1/org-roles');
+        $req->set_param('org_id', $this->org);
+        $res = rest_get_server()->dispatch($req);
+
+        $this->assertSame(200, $res->get_status());
+        $data = $res->get_data();
+        $this->assertSame($this->org, $data['org_id']);
+        $this->assertNotEmpty($data['roles']);
+        $this->assertCount(2, $data['users']);
+    }
+
+    public function test_update_roles_assigns_role(): void
+    {
+        $nonce = wp_create_nonce('wp_rest');
+        $_GET['_wpnonce'] = $nonce;
+        $req = new WP_REST_Request('POST', '/artpulse/v1/org-roles/update');
+        $req->set_body_params([
+            'org_id' => $this->org,
+            'roles'  => [ $this->user => 'admin' ],
+        ]);
+        $res = rest_get_server()->dispatch($req);
+
+        $this->assertSame(200, $res->get_status());
+        $this->assertSame('admin', get_user_meta($this->user, 'ap_org_role', true));
+    }
+}
