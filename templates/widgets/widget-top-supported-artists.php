b5351872f0788e04eaf26cfc956b2363993780fb
myspc-development <myspchosting@gmail.com>
2025-07-16 20:18:55 +0300

    Remove simplified widget templates and update registry

--- templates/widgets/widget-top-supported-artists.php
+++
@@ -1,8 +0,0 @@
-<div class="ap-card" role="region" aria-labelledby="ap-top-supported-title">
-    <h2 id="ap-top-supported-title" class="ap-card__title"><?php esc_html_e('Top Supported Artists','artpulse'); ?></h2>
-    <ul>
-        <?php foreach ($artists as $artist_id => $amount): ?>
-            <li><?php echo get_the_title($artist_id) . ' - $' . number_format($amount, 2); ?></li>
-        <?php endforeach; ?>
-    </ul>
-</div>
