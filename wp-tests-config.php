0b086ad368d142dfae2c1455e9cee1de02896c2f
myspc-development <myspchosting@gmail.com>
2025-07-18 11:00:05 +0300

    Use env vars for DB config

--- wp-tests-config.php
+++ wp-tests-config.php
@@ -4,10 +4,10 @@ define( 'WP_TESTS_EMAIL', 'admin@example.org' );
 define( 'WP_TESTS_TITLE', 'Test Blog' );
 define( 'WP_PHP_BINARY', 'php' ); // Adjust if using PHP 8.1+
 
-define( 'DB_NAME', 'wordpress_test' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', '' );
-define( 'DB_HOST', 'localhost' );
+define( 'DB_NAME', getenv( 'DB_NAME' ) ?: 'wordpress_test' );
+define( 'DB_USER', getenv( 'DB_USER' ) ?: 'root' );
+define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) ?: '' );
+define( 'DB_HOST', getenv( 'DB_HOST' ) ?: 'localhost' );
 $table_prefix = 'wptests_';
 
 // Required for some wp-phpunit setups
