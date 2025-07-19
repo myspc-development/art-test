3f977c4019044c422e02c5e7e109cb83aca50fda
myspc-development <myspchosting@gmail.com>
2025-07-19 08:50:28 +0300

    fix test config constant redefs

--- wp-tests-config.php
+++ wp-tests-config.php
@@ -3,19 +3,43 @@
  * WordPress test suite configuration file.
  */
 
-define( 'DB_NAME', getenv( 'DB_NAME' ) );
-define( 'DB_USER', getenv( 'DB_USER' ) );
-define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
-define( 'DB_HOST', getenv( 'DB_HOST' ) );
-define( 'DB_CHARSET', getenv( 'DB_CHARSET' ) );
-define( 'DB_COLLATE', getenv( 'DB_COLLATE' ) );
+// Allow environment variables or an existing test suite to set these constants.
+if ( ! defined( 'DB_NAME' ) ) {
+    define( 'DB_NAME', getenv( 'DB_NAME' ) );
+}
+if ( ! defined( 'DB_USER' ) ) {
+    define( 'DB_USER', getenv( 'DB_USER' ) );
+}
+if ( ! defined( 'DB_PASSWORD' ) ) {
+    define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
+}
+if ( ! defined( 'DB_HOST' ) ) {
+    define( 'DB_HOST', getenv( 'DB_HOST' ) );
+}
+if ( ! defined( 'DB_CHARSET' ) ) {
+    define( 'DB_CHARSET', getenv( 'DB_CHARSET' ) );
+}
+if ( ! defined( 'DB_COLLATE' ) ) {
+    define( 'DB_COLLATE', getenv( 'DB_COLLATE' ) );
+}
 
-define( 'WP_TESTS_DOMAIN', 'example.org' );
-define( 'WP_TESTS_EMAIL', 'admin@example.org' );
-define( 'WP_TESTS_TITLE', 'Test Blog' );
+if ( ! defined( 'WP_TESTS_DOMAIN' ) ) {
+    define( 'WP_TESTS_DOMAIN', 'example.org' );
+}
+if ( ! defined( 'WP_TESTS_EMAIL' ) ) {
+    define( 'WP_TESTS_EMAIL', 'admin@example.org' );
+}
+if ( ! defined( 'WP_TESTS_TITLE' ) ) {
+    define( 'WP_TESTS_TITLE', 'Test Blog' );
+}
 
-define( 'WP_PHP_BINARY', 'php' );
+if ( ! defined( 'WP_PHP_BINARY' ) ) {
+    define( 'WP_PHP_BINARY', 'php' );
+}
 
 define( 'ABSPATH', dirname( __FILE__ ) . '/wordpress/' );
+
+// Prefix for tables created during tests.
+$table_prefix = 'wptests_';
 
 require_once ABSPATH . '/wp-settings.php';
