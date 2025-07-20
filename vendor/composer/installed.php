2afcb0b7c0b00ee56193d53de3fe9565dce9f106
myspchosting <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>
2025-07-20 08:08:52 +0000

    Clean push after resolving file permission issues

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'myspc-development/artpulse-management-plugin',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '2900b66fdbbe4a9e11e92f05142dfb71ec166447',
+        'reference' => '942b3ec11dedc2bbbb4432500e9faae35efc92af',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -121,7 +121,7 @@
         'myspc-development/artpulse-management-plugin' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '2900b66fdbbe4a9e11e92f05142dfb71ec166447',
+            'reference' => '942b3ec11dedc2bbbb4432500e9faae35efc92af',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
