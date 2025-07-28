2356163ec5f97b1ea05b9ba75ef75e8809a71f79
myspc-development <myspchosting@gmail.com>
2025-07-28 09:09:30 +0200

    Refactor member dashboard layout

--- templates/dashboard-role.php
+++ templates/dashboard-role.php
@@ -2,5 +2,9 @@
 use ArtPulse\Core\DashboardController;
 
 $user_role = DashboardController::get_role(get_current_user_id());
+if ($user_role !== 'member') {
+    wp_die(__('Access denied', 'artpulse'));
+}
+
 include plugin_dir_path(__FILE__) . 'partials/dashboard-generic.php';
 
