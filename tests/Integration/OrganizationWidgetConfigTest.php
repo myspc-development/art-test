2e3a41e162d5be7990a2c02418a5980c6722d54f
myspc-development <myspchosting@gmail.com>
2025-08-02 11:08:57 +0200

    Register organization widgets

---
+++ tests/Integration/OrganizationWidgetConfigTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace ArtPulse\Integration\Tests;
+
+use WP_UnitTestCase;
+use ArtPulse\Core\WidgetRegistryLoader;
+use ArtPulse\Core\DashboardWidgetRegistry;
+
+class OrganizationWidgetConfigTest extends WP_UnitTestCase {
+    public function set_up(): void {
+        parent::set_up();
+
+        // reset registry and loader caches
+        $ref = new \ReflectionClass(DashboardWidgetRegistry::class);
+        $prop = $ref->getProperty('widgets');
+        $prop->setAccessible(true);
+        $prop->setValue([]);
+
+        $loaderRef = new \ReflectionClass(WidgetRegistryLoader::class);
+        $configProp = $loaderRef->getProperty('config');
+        $configProp->setAccessible(true);
+        $configProp->setValue([]);
+        $regProp = $loaderRef->getProperty('registered');
+        $regProp->setAccessible(true);
+        $regProp->setValue(false);
+
+        WidgetRegistryLoader::register_widgets();
+    }
+
+    public function test_org_widgets_registered_and_renderable(): void {
+        $ids = [
+            'audience_crm',
+            'sponsored_event_config',
+            'embed_tool',
+            'org_event_overview',
+            'org_team_roster',
+        ];
+
+        foreach ($ids as $id) {
+            $this->assertNotNull(
+                DashboardWidgetRegistry::get_widget($id),
+                "$id not registered"
+            );
+
+            ob_start();
+            ap_render_widget($id);
+            $html = ob_get_clean();
+            $this->assertStringContainsString($id, $html);
+        }
+    }
+}
