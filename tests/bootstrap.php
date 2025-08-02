76e71b1228614193d20aeafac65232cfd3d179a5
myspc-development <myspchosting@gmail.com>
2025-08-02 10:30:29 +0200

    Install WordPress testing package

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,6 +10,12 @@ if (!defined('WP_TESTS_PHPUNIT_POLYFILLS_PATH')) {
     );
 }
 
+// Load the WordPress test environment
+if ( ! defined( 'WP_TESTS_CONFIG_FILE_PATH' ) ) {
+    define( 'WP_TESTS_CONFIG_FILE_PATH', dirname( __DIR__ ) . '/wp-tests-config.php' );
+}
+require dirname(__DIR__) . '/vendor/wp-phpunit/wp-phpunit/includes/bootstrap.php';
+
 // Include function stubs used by tests
 require __DIR__ . '/TestHelpers/FunctionStubs.php';
 
