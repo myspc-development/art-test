1f490bece8fd0ac4614b8c102b3fd5dc0e83def4
myspc-development <myspchosting@gmail.com>
2025-08-04 21:23:30 +0200

    Fix PHPUnit bootstrap and require version

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,9 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $wp_bootstrap = dirname(__DIR__) . '/vendor/wp-phpunit/wp-phpunit/includes/bootstrap.php';
-if (file_exists($wp_bootstrap)) {
+$wp_settings = dirname(__DIR__) . '/wordpress/wp-settings.php';
+
+if (file_exists($wp_bootstrap) && file_exists($wp_settings)) {
     if (!defined('WP_TESTS_CONFIG_FILE_PATH')) {
         define('WP_TESTS_CONFIG_FILE_PATH', dirname(__DIR__) . '/wp-tests-config.php');
     }
@@ -10,8 +12,6 @@ if (file_exists($wp_bootstrap)) {
     tests_add_filter('muplugins_loaded', function () {
         require dirname(__DIR__) . '/artpulse.php';
     });
-} else {
-    if (!function_exists('tests_add_filter')) {
-        function tests_add_filter(...$args): void {}
-    }
+} elseif (!function_exists('tests_add_filter')) {
+    function tests_add_filter(...$args): void {}
 }
