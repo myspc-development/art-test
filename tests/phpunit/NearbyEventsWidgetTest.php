ed10afb841e10df7c00e1931fed904aabb278ea3
myspc-development <myspchosting@gmail.com>
2025-08-05 09:32:25 +0200

    Add NearbyEventsWidget for nearby events shortcode

---
+++ tests/phpunit/NearbyEventsWidgetTest.php
@@ -0,0 +1,21 @@
+<?php
+use ArtPulse\Widgets\NearbyEventsWidget;
+use PHPUnit\Framework\TestCase;
+
+if (!function_exists('do_shortcode')) {
+    function do_shortcode(string $content): string {
+        return str_replace('[near_me_events]', 'nearby events list', $content);
+    }
+}
+
+class NearbyEventsWidgetTest extends TestCase {
+    public function test_render_outputs_shortcode(): void {
+        $output = NearbyEventsWidget::render();
+
+        $this->assertStringContainsString(
+            'nearby events list',
+            $output,
+            'Widget should render the near_me_events shortcode.'
+        );
+    }
+}
