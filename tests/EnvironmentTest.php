663b503b7a93ec8dd459c964a1938c1b243dd817
myspc-development <myspchosting@gmail.com>
2025-08-15 17:45:32 +0200

    chore: configure wp-phpunit runner

---
+++ tests/EnvironmentTest.php
@@ -0,0 +1,9 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class EnvironmentTest extends TestCase {
+    public function test_wp_loaded() {
+        $this->assertTrue(defined('ABSPATH'), 'WordPress ABSPATH should be defined');
+        $this->assertTrue(function_exists('do_action'), 'Core functions should be loaded');
+    }
+}
