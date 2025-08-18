72781e69117a2f46c1533eded8ba0d07aef5a9bd
myspc-development <myspchosting@gmail.com>
2025-08-18 12:54:51 +0200

    test: add quick add event cypress spec

---
+++ cypress/e2e/dashboard_add_event.spec.js
@@ -0,0 +1,35 @@
+describe('Dashboard Quick Add Event button', () => {
+  const roles = ['artist', 'organization'];
+
+  roles.forEach((role) => {
+    describe(`${role} flow`, () => {
+      beforeEach(() => {
+        cy.login(role);
+        cy.visit('/dashboard');
+      });
+
+      it('shows Add New Event control', () => {
+        cy.get('#ap-add-event-btn').should('exist').click();
+
+        if (role === 'artist') {
+          cy.get('#ap-add-event-form').should('be.visible');
+
+          // mock network request to ensure submit triggers
+          cy.intercept('POST', '**/artpulse/v1/submissions', {
+            statusCode: 200,
+            body: {}
+          }).as('submitEvent');
+
+          cy.get('#ap-add-event-form').find('input[name="title"]').type('Cypress Test Event');
+          cy.get('#ap-add-event-form').find('button[type="submit"]').click();
+          cy.wait('@submitEvent');
+        } else {
+          cy.get('#ap-org-modal').should('exist').and(($el) => {
+            expect($el.attr('hidden')).to.be.undefined;
+          });
+          cy.get('#ap-org-event-form').should('exist');
+        }
+      });
+    });
+  });
+});
