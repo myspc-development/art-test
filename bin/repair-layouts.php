3d47f2daf337b7d288f7dda00c1751b9231d60c5
myspc-development <myspchosting@gmail.com>
2025-08-18 12:11:40 +0200

    Add WP-CLI command to repair dashboard layouts

---
+++ bin/repair-layouts.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use ArtPulse\Core\DashboardController;
+
+if (defined('WP_CLI') && WP_CLI) {
+    \WP_CLI::add_command('ap:repair_layouts', function () {
+        $users = get_users(['fields' => ['ID']]);
+        $repaired = 0;
+        foreach ($users as $user) {
+            if (DashboardController::reset_user_dashboard_layout((int) $user->ID)) {
+                $repaired++;
+            }
+        }
+        \WP_CLI::success("Repaired dashboard layouts for {$repaired} users.");
+    });
+} else {
+    fwrite(STDERR, "This script must be run within WP-CLI.\n");
+    exit(1);
+}
