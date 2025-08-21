bb8253623c4153f294c6d608104e2c044a1e9e8e
myspc-development <myspchosting@gmail.com>
2025-08-21 12:13:08 +0200

    Update Auth.php

--- src/Rest/Util/Auth.php
+++ src/Rest/Util/Auth.php
@@ -2,23 +2,28 @@
 namespace ArtPulse\Rest\Util;
 
 use WP_Error;
-use WP_REST_Request;
 
-/**
- * Generate a permission callback enforcing login and optional capability check.
- */
-function require_login_and_cap(callable $capCheck = null) {
-    return function (WP_REST_Request $request = null) use ($capCheck) {
-        if (!is_user_logged_in()) {
-            return new WP_Error(
-                'rest_not_logged_in',
-                __('Authentication required.'),
-                ['status' => rest_authorization_required_code()]
-            );
-        }
-        if ($capCheck && !$capCheck($request)) {
-            return new WP_Error('rest_forbidden', __('You do not have permission.'), ['status' => 403]);
-        }
-        return true;
-    };
+final class Auth {
+    /**
+     * Returns a permission_callback that enforces login and optional capability.
+     */
+    public static function require_login_and_cap(?callable $capCheck = null): callable {
+        return function() use ($capCheck) {
+            if ( ! is_user_logged_in() ) {
+                return new WP_Error(
+                    'rest_not_logged_in',
+                    __( 'Authentication required.', 'artpulse' ),
+                    [ 'status' => rest_authorization_required_code() ] // 401
+                );
+            }
+            if ( $capCheck && ! $capCheck() ) {
+                return new WP_Error(
+                    'rest_forbidden',
+                    __( 'You do not have permission.', 'artpulse' ),
+                    [ 'status' => 403 ]
+                );
+            }
+            return true;
+        };
+    }
 }
