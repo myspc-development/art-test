ed7ab8f2c8360dec71e8a362421accd289fc009b
myspc-development <myspchosting@gmail.com>
2025-08-23 12:58:11 +0200

    Add REST and admin nonce tests

---
+++ tests/Admin/ConfigBackupTabNonceTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace ArtPulse\Admin\Tests;
+
+use ArtPulse\Admin\ConfigBackupTab;
+
+class ConfigBackupTabNonceTest extends \WP_UnitTestCase
+{
+    private int $admin;
+
+    public function set_up(): void
+    {
+        parent::set_up();
+        $this->admin = self::factory()->user->create(['role' => 'administrator']);
+        wp_set_current_user($this->admin);
+        $_REQUEST = [];
+    }
+
+    public function tear_down(): void
+    {
+        $_REQUEST = [];
+        parent::tear_down();
+    }
+
+    public function test_handle_export_missing_nonce_fails(): void
+    {
+        $this->expectException(\WPDieException::class);
+        ConfigBackupTab::handle_export();
+    }
+
+    public function test_handle_export_invalid_nonce_fails(): void
+    {
+        $_REQUEST['_wpnonce'] = wp_create_nonce('invalid');
+        $this->expectException(\WPDieException::class);
+        ConfigBackupTab::handle_export();
+    }
+}
