b54cd2f97a2cc4c04e20247577bd4007b33ce514
myspc-development <myspchosting@gmail.com>
2025-08-23 17:39:53 +0200

    Add widget logger class with log level filter

--- tests/Core/WidgetLoggingTest.php
+++ tests/Core/WidgetLoggingTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace {
     // Simple hook system to mimic WordPress behavior.
-    $GLOBALS['ap_wp_hooks'] = ['actions' => [], 'filters' => []];
+    if (!isset($GLOBALS['ap_wp_hooks'])) {
+        $GLOBALS['ap_wp_hooks'] = ['actions' => [], 'filters' => []];
+    }
 
     if (!function_exists('add_filter')) {
         function add_filter($hook, $callback, $priority = 10, $args = 1) {
@@ -31,6 +33,11 @@ namespace {
     if (!function_exists('__return_true')) {
         function __return_true() { return true; }
     }
+    if (!function_exists('wp_upload_dir')) {
+        function wp_upload_dir() {
+            return ['basedir' => $GLOBALS['ap_wp_upload_dir']];
+        }
+    }
 }
 
 namespace ArtPulse\Core\Tests {
@@ -39,28 +46,30 @@ use PHPUnit\Framework\TestCase;
 
 class WidgetLoggingTest extends TestCase
 {
+    private string $uploadsDir;
     private string $logFile;
-    private string $origErrorLog;
 
     protected function setUp(): void
     {
         parent::setUp();
+        $GLOBALS['ap_wp_hooks'] = ['actions' => [], 'filters' => []];
         if (!defined('ABSPATH')) {
             define('ABSPATH', __DIR__);
         }
 
-        add_filter('ap_enable_widget_logging', '__return_true');
-        require_once __DIR__ . '/../../includes/widget-logging.php';
+        $this->uploadsDir = sys_get_temp_dir() . '/ap_uploads_' . uniqid();
+        mkdir($this->uploadsDir);
+        $GLOBALS['ap_wp_upload_dir'] = $this->uploadsDir;
+        $this->logFile = $this->uploadsDir . '/ap-widget.log';
 
-        $this->origErrorLog = ini_get('error_log');
-        $this->logFile = tempnam(sys_get_temp_dir(), 'ap_widget_log');
-        ini_set('error_log', $this->logFile);
+        add_filter('ap_enable_widget_logging', '__return_true');
+        include __DIR__ . '/../../includes/widget-logging.php';
     }
 
     protected function tearDown(): void
     {
-        ini_set('error_log', $this->origErrorLog);
         @unlink($this->logFile);
+        @rmdir($this->uploadsDir);
         parent::tearDown();
     }
 
@@ -69,9 +78,17 @@ class WidgetLoggingTest extends TestCase
         do_action('ap_widget_rendered', 'widget_id', 1);
         do_action('ap_widget_hidden', 'widget_id', 1);
 
+        $this->assertFileExists($this->logFile);
         $log = file_get_contents($this->logFile);
         $this->assertStringContainsString('Widget widget_id rendered for user 1', $log);
         $this->assertStringContainsString('Widget widget_id hidden for user 1', $log);
+    }
+
+    public function test_log_level_filter_is_respected(): void
+    {
+        add_filter('ap_widget_log_level', fn() => 'error');
+        do_action('ap_widget_rendered', 'widget_id', 1);
+        $this->assertFileDoesNotExist($this->logFile);
     }
 }
 
