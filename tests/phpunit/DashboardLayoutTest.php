d249ac04d89291380f2e2ca1bb4a32e54327d083
myspc-development <myspchosting@gmail.com>
2025-08-26 09:29:06 +0200

    Canonicalize widget IDs in layout tests

--- tests/phpunit/DashboardLayoutTest.php
+++ tests/phpunit/DashboardLayoutTest.php
@@ -64,88 +64,102 @@ class DashboardLayoutTest extends TestCase {
 
         $presets = DashboardController::get_default_presets();
         $this->assertSame('member', $presets['member_default']['role']);
-        $this->assertSame([['id' => 'widget_news']], $presets['member_default']['layout']);
+        $this->assertSame([
+            ['id' => 'widget_news', 'visible' => true],
+        ], $presets['member_default']['layout']);
         $this->assertSame('artist', $presets['artist_default']['role']);
-        $this->assertSame([['id' => 'artist_inbox_preview']], $presets['artist_default']['layout']);
+        $this->assertSame([
+            ['id' => 'artist_inbox_preview', 'visible' => true],
+        ], $presets['artist_default']['layout']);
     }
 
     public function test_fallback_layout_and_filtering(): void {
-        DashboardWidgetRegistry::register('alpha', 'Alpha', '', '', static function () {}, ['roles' => ['member']]);
-        DashboardWidgetRegistry::register('beta', 'Beta', '', '', static function () {}, ['roles' => ['member'], 'capability' => 'edit_posts']);
-        DashboardWidgetRegistry::register('gamma', 'Gamma', '', '', static function () {}, ['roles' => ['artist']]);
+        DashboardWidgetRegistry::register('widget_alpha', 'Alpha', '', '', static function () {}, ['roles' => ['member']]);
+        DashboardWidgetRegistry::register('widget_beta', 'Beta', '', '', static function () {}, ['roles' => ['member'], 'capability' => 'edit_posts']);
+        DashboardWidgetRegistry::register('widget_gamma', 'Gamma', '', '', static function () {}, ['roles' => ['artist']]);
 
         $ref2 = new \ReflectionClass(DashboardController::class);
         $prop2 = $ref2->getProperty('role_widgets');
         $prop2->setAccessible(true);
         $prop2->setValue(null, [
-            'member' => ['alpha', 'beta', 'gamma'],
+            'member' => ['widget_alpha', 'widget_beta', 'widget_gamma'],
         ]);
 
         MockStorage::$users[1] = (object)['roles' => ['member']];
         $layout = DashboardController::get_user_dashboard_layout(1);
-        $this->assertSame([['id' => 'alpha']], $layout);
+        $this->assertSame([
+            ['id' => 'widget_alpha', 'visible' => true],
+        ], $layout);
     }
 
     public function test_saved_layout_overrides_fallback(): void {
-        DashboardWidgetRegistry::register('alpha', 'Alpha', '', '', static function () {}, ['roles' => ['member']]);
-        DashboardWidgetRegistry::register('beta', 'Beta', '', '', static function () {}, ['roles' => ['member']]);
+        DashboardWidgetRegistry::register('widget_alpha', 'Alpha', '', '', static function () {}, ['roles' => ['member']]);
+        DashboardWidgetRegistry::register('widget_beta', 'Beta', '', '', static function () {}, ['roles' => ['member']]);
 
         $ref2 = new \ReflectionClass(DashboardController::class);
         $prop2 = $ref2->getProperty('role_widgets');
         $prop2->setAccessible(true);
-        $prop2->setValue(null, ['member' => ['beta']]);
+        $prop2->setValue(null, ['member' => ['widget_beta']]);
 
         MockStorage::$users[2] = (object)['roles' => ['member']];
-        MockStorage::$user_meta[2]['ap_dashboard_layout'] = [ ['id' => 'alpha'] ];
+        MockStorage::$user_meta[2]['ap_dashboard_layout'] = [ ['id' => 'widget_alpha', 'visible' => true] ];
         $layout = DashboardController::get_user_dashboard_layout(2);
-        $this->assertSame([['id' => 'alpha']], $layout);
+        $this->assertSame([
+            ['id' => 'widget_alpha', 'visible' => true],
+        ], $layout);
     }
 
     public function test_emits_action_when_layout_empty(): void {
-        DashboardWidgetRegistry::register('beta', 'Beta', '', '', static function () {}, ['roles' => ['artist']]);
+        DashboardWidgetRegistry::register('widget_beta', 'Beta', '', '', static function () {}, ['roles' => ['artist']]);
         $ref2 = new \ReflectionClass(DashboardController::class);
         $prop2 = $ref2->getProperty('role_widgets');
         $prop2->setAccessible(true);
-        $prop2->setValue(null, ['member' => ['beta']]);
+        $prop2->setValue(null, ['member' => ['widget_beta']]);
         MockStorage::$users[3] = (object)['roles' => ['member']];
         $layout = DashboardController::get_user_dashboard_layout(3);
-        $this->assertSame([['id' => 'empty_dashboard']], $layout);
+        $this->assertSame([
+            ['id' => 'empty_dashboard', 'visible' => true],
+        ], $layout);
         $this->assertSame([['ap_dashboard_empty_layout', [3, 'member']]], self::$actions);
     }
 
     public function test_preview_role_renders_layout(): void {
-        DashboardWidgetRegistry::register('alpha', 'Alpha', '', '', static function () {}, ['roles' => ['member']]);
-        DashboardWidgetRegistry::register('beta', 'Beta', '', '', static function () {}, ['roles' => ['artist']]);
+        DashboardWidgetRegistry::register('widget_alpha', 'Alpha', '', '', static function () {}, ['roles' => ['member']]);
+        DashboardWidgetRegistry::register('widget_beta', 'Beta', '', '', static function () {}, ['roles' => ['artist']]);
         $ref2 = new \ReflectionClass(DashboardController::class);
         $prop2 = $ref2->getProperty('role_widgets');
         $prop2->setAccessible(true);
         $prop2->setValue(null, [
-            'member' => ['alpha'],
-            'artist' => ['beta'],
+            'member' => ['widget_alpha'],
+            'artist' => ['widget_beta'],
         ]);
         MockStorage::$users[4] = (object)['roles' => ['administrator']];
         MockStorage::$current_roles = ['manage_options'];
         $_GET['ap_preview_role'] = 'artist';
         $layout = DashboardController::get_user_dashboard_layout(4);
         unset($_GET['ap_preview_role']);
-        $this->assertSame([['id' => 'beta']], $layout);
+        $this->assertSame([
+            ['id' => 'widget_beta', 'visible' => true],
+        ], $layout);
     }
 
     public function test_filter_accessible_layout_excludes_by_capability_and_role(): void {
-        DashboardWidgetRegistry::register('alpha', 'Alpha', '', '', static function () {}, ['roles' => ['member']]);
-        DashboardWidgetRegistry::register('beta', 'Beta', '', '', static function () {}, ['roles' => ['member'], 'capability' => 'edit_posts']);
-        DashboardWidgetRegistry::register('gamma', 'Gamma', '', '', static function () {}, ['roles' => ['artist']]);
+        DashboardWidgetRegistry::register('widget_alpha', 'Alpha', '', '', static function () {}, ['roles' => ['member']]);
+        DashboardWidgetRegistry::register('widget_beta', 'Beta', '', '', static function () {}, ['roles' => ['member'], 'capability' => 'edit_posts']);
+        DashboardWidgetRegistry::register('widget_gamma', 'Gamma', '', '', static function () {}, ['roles' => ['artist']]);
         $layout = [
-            ['id' => 'alpha'],
-            ['id' => 'beta'],
-            ['id' => 'gamma'],
-            ['id' => 'missing'],
+            ['id' => 'widget_alpha', 'visible' => true],
+            ['id' => 'widget_beta', 'visible' => true],
+            ['id' => 'widget_gamma', 'visible' => true],
+            ['id' => 'missing', 'visible' => true],
         ];
         $ref = new \ReflectionClass(DashboardController::class);
         $m = $ref->getMethod('filter_accessible_layout');
         $m->setAccessible(true);
         $filtered = $m->invoke(null, $layout, 'member');
-        $this->assertSame([['id' => 'alpha']], $filtered);
+        $this->assertSame([
+            ['id' => 'widget_alpha', 'visible' => true],
+        ], $filtered);
     }
 }
 }
