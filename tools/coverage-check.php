6d23cad0e27c48ec0dfa5bb01c1717579d912c4d
myspc-development <myspchosting@gmail.com>
2025-08-27 21:21:58 +0200

    Add coverage gating with PCOV/Xdebug fallback

---
+++ tools/coverage-check.php
@@ -0,0 +1,71 @@
+<?php
+if ($argc < 4) {
+    fwrite(STDERR, "Usage: coverage-check.php <coverage1.xml> [<coverage2.xml> ...] <overall> <Class=Threshold>...\n");
+    exit(1);
+}
+
+$files = [];
+// Collect coverage files
+while ($argc > 1 && file_exists($argv[1])) {
+    $files[] = array_shift($argv);
+    $argc--;
+}
+
+if (!$files) {
+    fwrite(STDERR, "No coverage files provided\n");
+    exit(1);
+}
+
+$overall = (float) array_shift($argv);
+$thresholds = [];
+foreach ($argv as $arg) {
+    if (strpos($arg, '=') === false) {
+        continue;
+    }
+    [$name, $min] = explode('=', $arg, 2);
+    $thresholds[$name] = (float) $min;
+}
+
+$totalStatements = 0;
+$totalCovered    = 0;
+$classMetrics    = [];
+
+foreach ($files as $file) {
+    $xml = new SimpleXMLElement(file_get_contents($file));
+    $metrics = $xml->project->metrics;
+    $totalStatements += (int) $metrics['statements'];
+    $totalCovered    += (int) $metrics['coveredstatements'];
+
+    foreach ($xml->xpath('//class') as $class) {
+        $name = (string) $class['name'];
+        $m    = $class->metrics;
+        $stat = (int) $m['statements'];
+        $cov  = (int) $m['coveredstatements'];
+        if (!isset($classMetrics[$name])) {
+            $classMetrics[$name] = ['statements' => 0, 'covered' => 0];
+        }
+        $classMetrics[$name]['statements'] += $stat;
+        $classMetrics[$name]['covered']    += $cov;
+    }
+}
+
+$overallPct = $totalStatements ? ($totalCovered / $totalStatements * 100) : 0;
+if ($overallPct < $overall) {
+    fwrite(STDERR, sprintf("Overall coverage %.2f%% is below required %.2f%%\n", $overallPct, $overall));
+    exit(1);
+}
+
+foreach ($thresholds as $class => $min) {
+    if (!isset($classMetrics[$class])) {
+        fwrite(STDERR, "Missing coverage for class $class\n");
+        exit(1);
+    }
+    $stat = $classMetrics[$class]['statements'];
+    $cov  = $classMetrics[$class]['covered'];
+    $pct  = $stat ? ($cov / $stat * 100) : 0;
+    if ($pct < $min) {
+        fwrite(STDERR, sprintf("Coverage for %s is %.2f%%, below required %.2f%%\n", $class, $pct, $min));
+        exit(1);
+    }
+}
+exit(0);
