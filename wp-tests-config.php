5c434eca5202a20485e9d2d65d17c303266be052
myspc-development <myspchosting@gmail.com>
2025-08-27 18:04:52 +0200

    Align WP test DB configuration

--- wp-tests-config.php
+++ wp-tests-config.php
@@ -5,22 +5,40 @@
 
 // Allow environment variables or an existing test suite to set these constants.
 if ( ! defined( 'DB_NAME' ) ) {
-    define( 'DB_NAME', getenv( 'DB_NAME' ) ?: 'wordpress_test' );
+    define(
+        'DB_NAME',
+        getenv( 'WP_TESTS_DB_NAME' ) ?: getenv( 'DB_NAME' ) ?: 'wordpress_test'
+    );
 }
 if ( ! defined( 'DB_USER' ) ) {
-    define( 'DB_USER', getenv( 'DB_USER' ) ?: 'wp' );
+    define(
+        'DB_USER',
+        getenv( 'WP_TESTS_DB_USER' ) ?: getenv( 'DB_USER' ) ?: 'wp'
+    );
 }
 if ( ! defined( 'DB_PASSWORD' ) ) {
-    define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) ?: 'password' );
+    define(
+        'DB_PASSWORD',
+        getenv( 'WP_TESTS_DB_PASSWORD' ) ?: getenv( 'DB_PASSWORD' ) ?: 'password'
+    );
 }
 if ( ! defined( 'DB_HOST' ) ) {
-    define( 'DB_HOST', getenv( 'DB_HOST' ) ?: '127.0.0.1' );
+    define(
+        'DB_HOST',
+        getenv( 'WP_TESTS_DB_HOST' ) ?: getenv( 'DB_HOST' ) ?: 'localhost'
+    );
 }
 if ( ! defined( 'DB_CHARSET' ) ) {
-    define( 'DB_CHARSET', getenv( 'DB_CHARSET' ) ?: 'utf8' );
+    define(
+        'DB_CHARSET',
+        getenv( 'WP_TESTS_DB_CHARSET' ) ?: getenv( 'DB_CHARSET' ) ?: 'utf8'
+    );
 }
 if ( ! defined( 'DB_COLLATE' ) ) {
-    define( 'DB_COLLATE', getenv( 'DB_COLLATE' ) ?: '' );
+    define(
+        'DB_COLLATE',
+        getenv( 'WP_TESTS_DB_COLLATE' ) ?: getenv( 'DB_COLLATE' ) ?: ''
+    );
 }
 
 if ( ! defined( 'WP_TESTS_DOMAIN' ) ) {
