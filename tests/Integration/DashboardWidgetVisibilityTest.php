7ccad67579f9a13364b8e22142142073972f642b
myspc-development <myspchosting@gmail.com>
2025-08-29 13:40:26 +0200

    Use canonical widget slugs in dashboard visibility tests

--- tests/Integration/DashboardWidgetVisibilityTest.php
+++ tests/Integration/DashboardWidgetVisibilityTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace ArtPulse\Integration\Tests;
 
+use ArtPulse\Admin\UserLayoutManager;
 use ArtPulse\Core\DashboardWidgetRegistry;
 
 class DashboardWidgetVisibilityTest extends \WP_UnitTestCase {
@@ -13,54 +14,64 @@ class DashboardWidgetVisibilityTest extends \WP_UnitTestCase {
 		delete_option( 'ap_widget_group_visibility' );
 	}
 
-	public function test_can_view_respected_in_render(): void {
-		DashboardWidgetRegistry::register_widget(
-			'yes',
-			array(
-				'label'    => 'Yes',
-				'callback' => array( TestWidgetYes::class, 'render' ),
-				'roles'    => array( 'member' ),
-			)
-		);
-		DashboardWidgetRegistry::register_widget(
-			'no',
-			array(
-				'label'    => 'No',
-				'callback' => array( TestWidgetNo::class, 'render' ),
-				'roles'    => array( 'member' ),
-			)
-		);
+       public function test_can_view_respected_in_render(): void {
+               DashboardWidgetRegistry::register_widget(
+                       'widget_yes',
+                       array(
+                               'label'    => 'Yes',
+                               'callback' => array( TestWidgetYes::class, 'render' ),
+                               'roles'    => array( 'member' ),
+                       )
+               );
+               DashboardWidgetRegistry::register_widget(
+                       'widget_no',
+                       array(
+                               'label'    => 'No',
+                               'callback' => array( TestWidgetNo::class, 'render' ),
+                               'roles'    => array( 'member' ),
+                       )
+               );
 
-		$uid = self::factory()->user->create( array( 'role' => 'member' ) );
-		wp_set_current_user( $uid );
-		ob_start();
-		DashboardWidgetRegistry::render_for_role( $uid );
-		$html = ob_get_clean();
+               UserLayoutManager::save_role_layout(
+                       'member',
+                       array(
+                               array( 'id' => 'widget_yes' ),
+                               array( 'id' => 'widget_no' ),
+                       )
+               );
 
-		$this->assertStringContainsString( 'YES', $html );
-		$this->assertStringNotContainsString( 'NO', $html );
-	}
+               $uid = self::factory()->user->create( array( 'role' => 'member' ) );
+               wp_set_current_user( $uid );
+               ob_start();
+               DashboardWidgetRegistry::render_for_role( $uid );
+               $html = ob_get_clean();
 
-	public function test_group_visibility_option_hides_widgets(): void {
-		DashboardWidgetRegistry::register_widget(
-			'grouped',
-			array(
-				'label'    => 'Grouped',
-				'callback' => array( TestWidgetYes::class, 'render' ),
-				'roles'    => array( 'member' ),
-				'group'    => 'beta',
-			)
-		);
-		update_option( 'ap_widget_group_visibility', array( 'beta' => false ) );
+               $this->assertStringContainsString( 'data-slug="widget_yes"', $html );
+               $this->assertStringNotContainsString( 'data-slug="widget_no"', $html );
+       }
 
-		$uid = self::factory()->user->create( array( 'role' => 'member' ) );
-		wp_set_current_user( $uid );
-		ob_start();
-		DashboardWidgetRegistry::render_for_role( $uid );
-		$html = ob_get_clean();
+       public function test_group_visibility_option_hides_widgets(): void {
+               DashboardWidgetRegistry::register_widget(
+                       'widget_yes',
+                       array(
+                               'label'    => 'Grouped',
+                               'callback' => array( TestWidgetYes::class, 'render' ),
+                               'roles'    => array( 'member' ),
+                               'group'    => 'beta',
+                       )
+               );
+               UserLayoutManager::save_role_layout( 'member', array( array( 'id' => 'widget_yes' ) ) );
+               update_option( 'ap_widget_group_visibility', array( 'beta' => false ) );
 
-		$this->assertStringNotContainsString( 'YES', $html );
-	}
+               $uid = self::factory()->user->create( array( 'role' => 'member' ) );
+               wp_set_current_user( $uid );
+               ob_start();
+               DashboardWidgetRegistry::render_for_role( $uid );
+               $html = ob_get_clean();
+
+               $this->assertStringNotContainsString( 'data-slug="widget_yes"', $html );
+               $this->assertStringNotContainsString( 'YES', $html );
+       }
 }
 
 class TestWidgetYes {
