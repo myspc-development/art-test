481aab6e1edfa5959adce7ec8b7ffce0c7fcb490
myspc-development <myspchosting@gmail.com>
2025-08-29 11:48:18 +0200

    Add auth checks for portfolio REST routes

--- tests/Rest/PortfolioPermissionsTest.php
+++ tests/Rest/PortfolioPermissionsTest.php
@@ -1,12 +1,27 @@
 <?php
 
+use ArtPulse\Rest\PortfolioRestController;
 
 class PortfolioPermissionsTest extends WP_UnitTestCase {
+	public function set_up() {
+		parent::set_up();
+		PortfolioRestController::register();
+		do_action( 'rest_api_init' );
+	}
+
 	public function test_requires_authentication() {
 		wp_set_current_user( 0 );
-		$request  = new \WP_REST_Request( 'GET', '/artpulse/v1/portfolio/1' );
+		$request  = new \WP_REST_Request( 'GET', '/ap/v1/portfolio' );
 		$response = rest_get_server()->dispatch( $request );
 		$status   = $response->get_status();
 		$this->assertTrue( in_array( $status, array( 401, 403 ), true ) );
+	}
+
+	public function test_allows_authenticated_user() {
+		$user_id = self::factory()->user->create( array( 'role' => 'artist' ) );
+		wp_set_current_user( $user_id );
+		$request  = new \WP_REST_Request( 'GET', '/ap/v1/portfolio' );
+		$response = rest_get_server()->dispatch( $request );
+		$this->assertSame( 200, $response->get_status() );
 	}
 }
