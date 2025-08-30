246e2a5df3753fbb7da01c599963353d9b43c819
myspc-development <myspchosting@gmail.com>
2025-08-30 12:46:58 +0200

    Use Auth util for current user permission

--- src/Rest/CurrentUserController.php
+++ src/Rest/CurrentUserController.php
@@ -1,6 +1,8 @@
 <?php
 namespace ArtPulse\Rest;
 
+use ArtPulse\Rest\Util\Auth;
+
 class CurrentUserController {
 	public static function register(): void {
 		add_action( 'rest_api_init', array( self::class, 'register_routes' ) );
@@ -14,11 +16,12 @@ class CurrentUserController {
                                array(
                                        'methods'             => 'GET',
                                        'callback'            => array( self::class, 'get_current_user' ),
-                                       'permission_callback' => function () {
-                                               if (defined('AP_TESTING') && AP_TESTING) {
+                                       'permission_callback' => function ( $req ) {
+                                               if ( defined( 'AP_TEST_MODE' ) && AP_TEST_MODE ) {
                                                        return is_user_logged_in();
                                                }
-                                               return current_user_can('manage_options') || current_user_can('edit_posts');
+                                               $permission = Auth::require_login_and_cap( array( 'manage_options', 'edit_posts' ) );
+                                               return $permission( $req );
                                        },
                                )
                         );
