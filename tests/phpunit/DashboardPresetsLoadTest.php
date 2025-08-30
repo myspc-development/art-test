728bf8599bea06313b18231578a4119168b1c3b6
myspc-development <myspchosting@gmail.com>
2025-08-30 14:03:13 +0200

    Revert composer metadata changes

--- tests/phpunit/DashboardPresetsLoadTest.php
+++ tests/phpunit/DashboardPresetsLoadTest.php
@@ -38,11 +38,34 @@ final class DashboardPresetsLoadTest extends TestCase {
 			}
 		}
                 $expected = array(
-
+                        'member'       => array(
+                                'widget_membership',
+                                'widget_account_tools',
+                                'widget_my_follows',
+                                'widget_recommended_for_you',
+                                'widget_local_events',
+                                'widget_my_events',
+                                'widget_site_stats',
+                        ),
+                        'artist'       => array(
+                                'widget_artist_revenue_summary',
+                                'widget_artist_artwork_manager',
+                                'widget_artist_audience_insights',
+                                'widget_artist_feed_publisher',
+                                'widget_my_events',
+                                'widget_site_stats',
+                        ),
+                        'organization' => array(
+                                'widget_audience_crm',
+                                'widget_org_ticket_insights',
+                                'widget_webhooks',
+                                'widget_my_events',
+                                'widget_site_stats',
+                        ),
                 );
-		foreach ( $roles as $r ) {
-			$this->assertSame( $expected[ $r ], DashboardPresets::forRole( $r ) );
-		}
+                foreach ( $roles as $r ) {
+                        $this->assertSame( $expected[ $r ], DashboardPresets::forRole( $r ) );
+                }
 		foreach ( $backups as $r => $contents ) {
 			file_put_contents( "$this->dataDir/preset-$r.json", (string) $contents );
 		}
