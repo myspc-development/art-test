d983ff544cb762dbd2ce0f0dced8bddbfd13a7a5
myspc-development <myspchosting@gmail.com>
2025-08-30 08:07:36 +0200

    Use namespaced AjaxTestHelper in tests

--- tests/Integration/LayoutSaveEndpointAjaxTest.php
+++ tests/Integration/LayoutSaveEndpointAjaxTest.php
@@ -2,7 +2,7 @@
 namespace ArtPulse\Integration\Tests;
 
 use WP_Ajax_UnitTestCase;
-use AjaxTestHelper;
+use ArtPulse\Tests\AjaxTestHelper;
 use ArtPulse\Core\DashboardWidgetRegistry;
 
 class LayoutSaveEndpointAjaxTest extends WP_Ajax_UnitTestCase {
