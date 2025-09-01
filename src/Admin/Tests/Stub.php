dc65b58c24e575eaaba6fa6938d42d2947e987a6
myspc-development <myspchosting@gmail.com>
2025-09-01 11:04:06 +0200

    Add OrgRoles REST controller and admin tests stub

---
+++ src/Admin/Tests/Stub.php
@@ -0,0 +1,11 @@
+<?php
+namespace ArtPulse\Admin\Tests;
+
+final class Stub {
+        public static function metrics(): array {
+                return array(
+                        'views' => 0,
+                        'likes' => 0,
+                );
+        }
+}
---
+++ src/Rest/OrgRolesController.php
@@ -0,0 +1,30 @@
+<?php
+namespace ArtPulse\Rest;
+
+use WP_REST_Response;
+use ArtPulse\Rest\Util\Auth;
+use ArtPulse\Rest\RestResponder;
+
+class OrgRolesController {
+        use RestResponder;
+
+        public static function register_routes(): void {
+                register_rest_route(
+                        ARTPULSE_API_NAMESPACE,
+                        '/org-roles',
+                        array(
+                                'methods'             => 'GET',
+                                'callback'            => array( self::class, 'get_roles' ),
+                                'permission_callback' => array( Auth::class, 'guard_read' ),
+                        )
+                );
+        }
+
+        public static function get_roles(): WP_REST_Response {
+                return \rest_ensure_response(
+                        array(
+                                'roles' => array( 'owner', 'manager', 'editor', 'viewer' ),
+                        )
+                );
+        }
+}
