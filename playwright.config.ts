1ef7725a232558a0ffda4c19d27504e4c3384743
myspc-development <myspchosting@gmail.com>
2025-09-02 11:33:26 +0200

    chore: add junit reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: 'tests/playwright',
   reporter: [
-    ['html', { open: 'never' }],
-    ['list']
+    ['html', { open: 'never', outputFolder: 'build/e2e/playwright-report' }],
+    ['list'],
+    ['junit', { outputFile: 'build/e2e/playwright.xml' }]
   ],
   expect: {
     toHaveScreenshot: {
