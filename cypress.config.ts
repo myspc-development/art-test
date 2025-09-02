c7abbf282d9961c36b9479e7ae6f6ee0f56e470e
myspc-development <myspchosting@gmail.com>
2025-09-02 11:35:20 +0200

    chore: configure cypress junit reporting

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'spec, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'build/e2e/cypress.xml'
+    }
+  },
+  videosFolder: 'build/e2e/videos',
+  screenshotsFolder: 'build/e2e/screenshots',
+  trashAssetsBeforeRuns: false,
+  video: true,
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:8080',
+    screenshotOnRunFailure: true,
     env: {
       ARTIST_USER: process.env.CYPRESS_ARTIST_USER || 'artist',
       PUBLIC_USER: process.env.CYPRESS_PUBLIC_USER || 'public_user',
