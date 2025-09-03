5d75cfc9901df98dfaa63b0391266dd7a12e3380
myspc-development <myspchosting@gmail.com>
2025-09-03 08:13:24 +0200

    Check block registration before hook and clean up test registry

--- tests/Blocks/RelatedProjectsBlockTest.php
+++ tests/Blocks/RelatedProjectsBlockTest.php
@@ -8,6 +8,11 @@ use WP_Block_Type_Registry;
  * @group BLOCKS
  */
 class RelatedProjectsBlockTest extends \WP_UnitTestCase {
+    protected function tearDown(): void {
+        WP_Block_Type_Registry::get_instance()->unregister( 'artpulse/related-projects' );
+        parent::tearDown();
+    }
+
     public function test_register_block_is_idempotent(): void {
         $triggered = false;
         $listener = function ( $function, $message ) use ( &$triggered ) {
@@ -23,8 +28,6 @@ class RelatedProjectsBlockTest extends \WP_UnitTestCase {
         RelatedProjectsBlock::register_block();
 
         \remove_action( 'doing_it_wrong_run', $listener, 10 );
-        WP_Block_Type_Registry::get_instance()->unregister( 'artpulse/related-projects' );
-
         $this->assertFalse( $triggered, 'Incorrect-usage notice triggered.' );
     }
 }
