aa814e111779c567482951f53f1ad97bffa6aa27
myspc-development <myspchosting@gmail.com>
2025-09-03 07:35:04 +0200

    Avoid duplicate Event List block registration

--- src/Blocks/EventListBlock.php
+++ src/Blocks/EventListBlock.php
@@ -9,15 +9,26 @@ class EventListBlock {
 	}
 
 	public static function register_block(): void {
-		if ( ! function_exists( 'register_block_type_from_metadata' ) ) {
-			return;
-		}
-		register_block_type_from_metadata(
-			__DIR__ . '/../../blocks/event-list',
-			array(
-				'render_callback' => array( self::class, 'render_callback' ),
-			)
-		);
+        if ( ! function_exists( 'register_block_type_from_metadata' ) ) {
+                return;
+        }
+
+        if ( function_exists( 'wp_is_block_registered' ) ) {
+                if ( wp_is_block_registered( 'artpulse/event-list' ) ) {
+                        return;
+                }
+        } else {
+                if ( \WP_Block_Type_Registry::get_instance()->is_registered( 'artpulse/event-list' ) ) {
+                        return;
+                }
+        }
+
+        register_block_type_from_metadata(
+                __DIR__ . '/../../blocks/event-list',
+                array(
+                        'render_callback' => array( self::class, 'render_callback' ),
+                )
+        );
 	}
 
 	public static function render_callback( $attributes ) {
