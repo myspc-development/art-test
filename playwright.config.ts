0e2b82cf72a9b0f559986a84f3cde7ec69001e7d
myspc-development <myspchosting@gmail.com>
2025-09-04 09:59:47 +0200

    chore: enhance playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,33 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-  testDir: 'tests/playwright',
-  reporter: [
-    ['junit', { outputFile: 'build/e2e/junit.xml' }],
-    ['html', { open: 'never', outputFolder: 'build/e2e/html' }]
-  ],
+  testDir: './e2e',
+  timeout: 30 * 1000,
   expect: {
+    timeout: 5 * 1000,
     toHaveScreenshot: {
       maxDiffPixelRatio: 0.001,
     },
   },
+  reporter: [
+    ['junit', { outputFile: 'build/e2e/junit.xml' }],
+    ['html', { open: 'never', outputFolder: 'build/e2e/html' }],
+  ],
+  use: {
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+  outputDir: 'build/e2e/artifacts',
+  globalSetup: './e2e/global-setup.ts',
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
 });
+
